0f6cc496730c7bff2623ee9f9ab548b9a07fc9c0
kk <liuyukai0324@outlook.com>
2025-06-15 21:36:47 +0800

    提交

--- db/db.js
+++ db/db.js
@@ -1,7 +1,6 @@
 module.exports = (success, error) => {
     const mongoose = require("mongoose")
     mongoose.connect("mongodb://localhost:27017/projects")
-
     mongoose.connection.once("open", () => {
         success()
     })
